ffbb7428f753c01fc7ae817d8e356414202dcf3e
engj3ll <arberpr6@gmail.com>
2021-06-20 16:25:53 +0200

    Added SQLite support for RegisterActivity

--- app/src/main/java/fiek/fiekunipr/bookism/RegisterActivity.java
+++ app/src/main/java/fiek/fiekunipr/bookism/RegisterActivity.java
@@ -2,13 +2,61 @@ package fiek.fiekunipr.bookism;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText name, surname, password, repass, email;
+    Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        name = name.findViewById(R.id.name);
+        password = password.findViewById(R.id.password);
+        repass = password.findViewById(R.id.repassword);
+        surname = surname.findViewById(R.id.surname);
+        signIn = signIn.findViewById(R.id.btnsignup);
+        email = email.findViewById(R.id.email);
+
+        String nameString = name.getText().toString().trim();
+        String surnameS = surname.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        String reepass = repass.getText().toString().trim();
+        String sEmail = email.getText().toString().trim();
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(nameString) && !TextUtils.isEmpty(surnameS) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(reepass) && !TextUtils.isEmpty(sEmail)) {
+                    if (password.getText().toString().equals(repass.getText().toString())) {
+                        SQLiteDatabase objDb = new DatabaseHelper(RegisterActivity.this).getWritableDatabase();
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(DatabaseModelHelper.UsersName, name.getText().toString());
+                        contentValues.put(DatabaseModelHelper.UsersLastname, surname.getText().toString());
+                        contentValues.put(DatabaseModelHelper.UsersEmail, email.getText().toString());
+                        contentValues.put(DatabaseModelHelper.UsersPassword, password.getText().toString());
+
+                        try {
+                            long id = objDb.insert(DatabaseModelHelper.UsersTable, null, contentValues);
+                            if (id > 0)
+                                Toast.makeText(RegisterActivity.this, getString(R.string.success_message), Toast.LENGTH_LONG).show();
+                        } catch (Exception ex) {
+                            Toast.makeText(RegisterActivity.this, ex.getMessage(), Toast.LENGTH_LONG).show();
+                        } finally {
+                            objDb.close();
+                        }
+                    }
+                }
+            }
+        });
     }
 }
