3c99074ec13d7ea41534f69bbb0e04423aa95537
engj3ll <arberpr6@gmail.com>
2021-06-20 13:37:57 +0200

    Added shared preferences

--- app/src/main/java/fiek/fiekunipr/bookism/LoginActivity.java
+++ app/src/main/java/fiek/fiekunipr/bookism/LoginActivity.java
@@ -3,6 +3,7 @@ package fiek.fiekunipr.bookism;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -40,6 +41,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    protected void onResume() {
+        super.onResume();
+
+        // Fetching the stored data
+        // from the SharedPreference
+        SharedPreferences sh = getSharedPreferences("LoginPreferences", MODE_PRIVATE);
+
+        String s1 = sh.getString("email", "");
+        String s2 = sh.getString("password", "");
+
+        // Setting the fetched data
+        // in the EditTexts
+        email.setText(s1);
+        password.setText(s2);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Creating a shared pref object
+        // with a file name "MySharedPref"
+        // in private mode
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+        // write all the data entered by the user in SharedPreference and apply
+        myEdit.putString("email", email.getText().toString());
+        myEdit.putString("password", password.getText().toString());
+        myEdit.apply();
+    }
+
+    // Store the data in the SharedPreference
+    // in the onPause() method
+    // When the user closes the application
+    // onPause() will be called
+    // and data will be stored
     private int LoginUser(String email, String password)
     {
         SQLiteDatabase objDb = new DatabaseHelper(LoginActivity.this).getReadableDatabase();
