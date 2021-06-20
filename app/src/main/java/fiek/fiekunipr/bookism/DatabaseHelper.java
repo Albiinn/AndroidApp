4088fe602c1032a0c10d7e3b7dff89a016a17df3
engj3ll <arberpr6@gmail.com>
2021-06-20 12:55:35 +0200

    Added login activity:

--- app/src/main/java/fiek/fiekunipr/bookism/DatabaseHelper.java
+++ app/src/main/java/fiek/fiekunipr/bookism/DatabaseHelper.java
@@ -5,14 +5,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(@Nullable Context context) {
-        super(context, "BookismDB", null, 1);
+        super(context, "FiekDB", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table Users(Id INTEGER PRIMARY KEY AUTOINCREMENT,Name TEXT, Lastname TEXT, Email TEXT, Password TEXT)");
+        db.execSQL("create table Users(Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, LastName TEXT, Email TEXT, Password TEXT)");
     }
 
     @Override
--- app/src/main/java/fiek/fiekunipr/bookism/MainActivity.java
+++ app/src/main/java/fiek/fiekunipr/bookism/MainActivity.java
@@ -2,6 +2,7 @@ package fiek.fiekunipr.bookism;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginActivityIntent);
+            }
+        });
 
     }
 }
