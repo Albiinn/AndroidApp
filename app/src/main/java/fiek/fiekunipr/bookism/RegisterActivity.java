3cf64f0a0bd97d9e171ccc09809db5a5361b6b11
engj3ll <arberpr6@gmail.com>
2021-06-22 09:58:11 +0200

    Added Firebase integration

--- app/src/main/java/fiek/fiekunipr/bookism/RegisterActivity.java
+++ app/src/main/java/fiek/fiekunipr/bookism/RegisterActivity.java
@@ -1,5 +1,6 @@
 package fiek.fiekunipr.bookism;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
@@ -7,14 +8,25 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
     EditText name, surname, password, repass, email;
     Button signIn;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,39 +40,85 @@ public class RegisterActivity extends AppCompatActivity {
         signIn = findViewById(R.id.btnsignup);
         email = findViewById(R.id.email);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    registerUser();
+                    }
+//                }
+//            }
+        });
+    }
+    private void registerUser() {
         String nameString = name.getText().toString().trim();
         String surnameS = surname.getText().toString().trim();
         String pass = password.getText().toString().trim();
         String reepass = repass.getText().toString().trim();
         String sEmail = email.getText().toString().trim();
 
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                if (!TextUtils.isEmpty(nameString) && !TextUtils.isEmpty(surnameS) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(reepass) && !TextUtils.isEmpty(sEmail)) {
-//                    if (password.getText().toString().equals(repass.getText().toString())) {
-                        SQLiteDatabase objDb = new DatabaseHelper(RegisterActivity.this).getWritableDatabase();
-                        ContentValues contentValues = new ContentValues();
-                        contentValues.put(DatabaseModelHelper.UsersName, name.getText().toString());
-                        contentValues.put(DatabaseModelHelper.UsersLastname, surname.getText().toString());
-                        contentValues.put(DatabaseModelHelper.UsersEmail, email.getText().toString());
-                        contentValues.put(DatabaseModelHelper.UsersPassword, password.getText().toString());
+        if(nameString.isEmpty()) {
+            name.setError("Name is required");
+            name.requestFocus();
+            return;
+        }
+        if(surnameS.isEmpty()) {
+            surname.setError("Surname is required");
+            surname.requestFocus();
+            return;
+        }
+        if(pass.isEmpty()) {
+            password.setError("Name is required");
+            password.requestFocus();
+            return;
+        }
+        if(reepass.isEmpty()) {
+            repass.setError("Name is required");
+            repass.requestFocus();
+            return;
+        }
+        if(sEmail.isEmpty()) {
+            email.setError("Name is required");
+            email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(sEmail).matches()) {
+            email.setError("Enter right email");
+            email.requestFocus();
+            return;
+        }
+        if(reepass.length()<6) {
+            password.setError("Password must be bigger than 6 characters");
+            password.requestFocus();
+            return;
+        }
 
-                        try {
-                            long id = objDb.insert(DatabaseModelHelper.UsersTable, null, contentValues);
-                            if (id > 0)
-                                Toast.makeText(RegisterActivity.this, getString(R.string.success_message), Toast.LENGTH_LONG).show();
-                            Intent feedActivity = new Intent(RegisterActivity.this, FeedActivity.class);
-                            startActivity(feedActivity);
-
-                        } catch (Exception ex) {
-                            Toast.makeText(RegisterActivity.this, ex.getMessage(), Toast.LENGTH_LONG).show();
-                        } finally {
-                            objDb.close();
+        mAuth.createUserWithEmailAndPassword(sEmail, pass)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            UserHelper user = new UserHelper(nameString, surnameS, sEmail);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(task1 -> {
+                                        if(task1.isSuccessful()){
+                                            Toast.makeText(RegisterActivity.this, "User has been registered successfully", Toast.LENGTH_LONG).show();
+                                        }
+                                        else{
+                                            Toast.makeText(RegisterActivity.this, "Failed to Register", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
                         }
-                    }
-//                }
-//            }
-        });
+                        else{
+                            Toast.makeText(RegisterActivity.this, "Failed Everything", Toast.LENGTH_LONG).show();
+                        }
+                        }
+//
+                });
+
     }
 }
+
