20d41836a2352025a230174cba8a78dd245143b5
Albiinn <albinbajrami15@hotmail.com>
2021-10-13 11:21:18 +0200

    update manifest

--- app/src/main/java/com/unipr/bookblog/Activities/PostDetailActivity.java
+++ app/src/main/java/com/unipr/bookblog/Activities/PostDetailActivity.java
@@ -26,6 +26,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.unipr.bookblog.Adapters.CommentAdapter;
 import com.unipr.bookblog.Models.Comment;
 import com.unipr.bookblog.R;
 
